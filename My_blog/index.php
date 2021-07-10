9726f75b28133f382b832a30a98b873aabc6bad8
PRO <39678394+Protap-mistry@users.noreply.github.com>
2021-07-10 13:04:01 +0600

    My blog

---
+++ My_blog/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Blog</title>
+	<link rel="stylesheet" href="style.css?v=<?php echo time()?>">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap">
+</head>
+<body>
+	<header>
+		<section id="sec">
+			<div id="logo">
+				<h1>#Tech_PRO</h1>
+			</div>
+			<nav id="navbar">
+				<ul>
+					<li><a href="#" title="" id="first">Abode</a></li>
+					<li><a href="#" title="">Biography</a></li>
+					<li><a href="#" title="">Courses</a></li>
+					<li><a href="#" title="">Experiences</a></li>
+					<li><a href="#" title="">Training/Workshop Attended</a></li>
+					<li><a href="#" title="">Interest</a></li>
+					<li><a href="#" title="">Extra Curricular Activities</a></li>
+					<li><a href="#" title="">References</a></li>
+				</ul>
+			</nav>
+		</section>
+		<main>
+			<div id="left">
+				<h1>Hello!!! Guest(s)</h1>
+				<h2>Here, Protap Mistry</h2>
+				<h3>Web Developer</h3>
+
+				<a href="#" id="button">Hire Me</a>
+			</div>
+			<div id="right">
+				<img src="images/pro.jpg" alt="Author">
+			</div>
+		</main>
+	</header>
+
+	<div id="fixed_icon">
+		<a href="http://www.facebook.com" title="Facebook" target="_blank"><img src="images/facebook.png" alt="Facebook"></a>
+		<a href="http://www.google.com" title="Google" target="_blank"><img src="images/google.png" alt="Google"></a>
+		<a href="http://www.linkedin.com" title="Linked In" target="_blank"><img src="images/linkedin.png" alt="Linked In"></a>
+		<a href="http://www.twitter.com" title="Twitter" target="_blank"><img src="images/twitter.png" alt="Twitter"></a>
+		<a href="http://www.youtube.com" title="Youtube" target="_blank"><img src="images/youtube.png" alt="Youtube"></a>
+	</div>
+</body>
+</html>
